bf8bdd318bed542e26a8ed97d8fcf3e5aadcee06
hjhan <hjhan@syscon.kr>
2022-10-26 19:16:56 +0900

    feat : 문자열 11720, 10809

--- src/Baekjoon/bronze/StringQuestion.java
+++ src/Baekjoon/bronze/StringQuestion.java
@@ -1,21 +1,63 @@
 package Baekjoon.bronze;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StringQuestion {
     public static void main(String[] args) {
-        QOne();
+        QThree();
     }
 
     /**
      * 11654, 아스키 코드
      */
     public static void QOne(){
-        // 1. String으로 입력받고 charAt()으로 짜른다(문자열을 잘라 문자로 변환)
-        // 2. int 타입 변수에 저장함으로써 문자에 대응되는값, 아스키 코드를 반환
         Scanner scan = new Scanner(System.in);
-        int str = scan.next().charAt(0);
-        System.out.println(str);
+        int num = scan.next().charAt(0);
+        System.out.println(num);
     }
 
+    /**
+     * 11720, 숫자들의 합
+     */
+    public static void QTwo(){
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        int cnt = scan.nextInt();
+        String numbers = scan.next();
+        for(int i = 0; i < cnt; i++){
+            /**이렇게 했을 때 안되는 이유 : charAt()은 해당 문자의 아스키 코드값을 반환하므로 반드시 -48 또는 '0'을 해줘야 우리가 입력한 값 그대로를 반환한다*/
+//            sum += numbers.charAt(i);
+            sum += numbers.charAt(i)-48;
+        }
+        System.out.println(sum);
+    }
+
+    /**
+     * 10809, 알파벳 찾기
+     * 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력
+     */
+    public static void QThree() {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int cnt = 0;
+        int[] arr = new int[26];    // a~ z : 26자
+
+        //초기값 셋팅
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = -1;
+        }
+
+        for(int i = 0; i<s.length(); i++){
+            int ss = s.charAt(i)-'a';   // ss가 곧 a-z 배열에 해당하는 인덱스
+            if(arr[ss] == -1){
+                arr[ss] = i;
+            }
+        }
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
